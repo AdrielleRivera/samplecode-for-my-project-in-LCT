b96ed7f0a4f11c4c0c3c63d98363a497c28886a0
Adrielle Rivera <mr.adriellerivera@gmail.com>
2024-08-08 17:46:11 +0800

    Update main.js
    Removed Adriii in prompt

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var password;
 var pass1 = "Adriii";
-password = prompt("Please enter your password to view this page! Adriii");
+password = prompt("Please enter your password to view this page!");
 	if (password==pass1)
 	{
 		alert("Password is Correct! Click OK to enter!");
