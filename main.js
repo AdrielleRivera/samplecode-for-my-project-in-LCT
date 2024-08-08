b344ce5dd884df838299fa8ad00edcca3fadd052
Adrielle Rivera <mr.adriellerivera@gmail.com>
2024-08-08 17:40:55 +0800

    Update main.js
    added my password in the prompt

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var password;
 var pass1 = "Adriii";
-password = prompt("Please enter your password to view this page!");
+password = prompt("Please enter your password to view this page! It's Adriii");
 	if (password==pass1)
 	{
 		alert("Password is Correct! Click OK to enter!");
