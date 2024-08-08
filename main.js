a7521b710ac52c6a27cba35bb6b211069ebb7122
Adrielle Rivera <mr.adriellerivera@gmail.com>
2024-08-08 17:43:39 +0800

    Update main.js
    test

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var password;
 var pass1 = "Adriii";
-password = prompt("Please enter your password to view this page! It's Adriii");
+password = prompt("Please enter your password to view this page! Adriii");
 	if (password==pass1)
 	{
 		alert("Password is Correct! Click OK to enter!");
